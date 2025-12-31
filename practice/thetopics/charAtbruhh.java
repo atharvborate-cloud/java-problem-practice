d69276f4d133bf0789a50671a7d705096e3f3428
atharvborate-cloud <atharvborate2004@gamil.com>
2025-12-31 19:57:55 +0530

    new topics

---
+++ practice/thetopics/charAtbruhh.java
@@ -0,0 +1,15 @@
+package practice.thetopics;
+
+public class charAtbruhh {
+    public static void main(String[] args) {
+        String s = "Atharv";
+
+        for (int i = 0; i < s.length();i++){
+//            System.out.print(s.charAt(i) + " " + "and" + " "); ye ek tarreka hai but not used in dsa and cp
+            char ch = s.charAt(i);
+            System.out.println(ch + " " + "and" );
+
+        }
+
+    }
+}
